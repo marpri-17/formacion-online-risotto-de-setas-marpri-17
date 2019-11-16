c9a81bea36bfab061543dce134085de0b53870d1
marpri-17 <mar.pnieto@gmail.com>
2019-11-16 13:03:28 +0100

    create function to personalize mix

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,9 +1,12 @@
 'use strict';
+
+
 const recipesPath = "./assets/services/recipes.json"
 const mixCheckboxes = document.querySelectorAll(".js-mix-check")
 const mixIngredientsList = document.querySelector(".js-list")
 
 let availableRecipes = [];
+let partialItemsAndQuantity = []
 let testValueSelectedRecipe = "recipe-01"
 
 function getRecipes() {
@@ -13,7 +16,8 @@ function getRecipes() {
             data = formatData(data);
             saveDatainRecipes(data);
             // renderOptionsForRecipes(data) // return select value matching item;
-            paintMixForSelectedOption(data[0])
+            paintMixForSelectedOption(data[0]);
+            readCheckboxesItemsandQuantity() //data[0];
 
 
 
@@ -36,54 +40,40 @@ function paintMixForSelectedOption(selectedRecipe) {
     const currency = selectedRecipe.currency;
     selectedRecipe.ingredients.map(ingredient => {
         const html = `
-        <input class="form-check-input js-mix-check" type="checkbox" value="" id="">
-        <input type="number" class="" id="selectQuantity"value= 1 />
+        <input class="form-check-input check js-mix-check" type="checkbox" value="" id="" checked>
+        <input type="number" class="js-quantity" id="selectQuantity"value= 1 />
         <label for="selectQuantity" class="sr-only">Cantidad</label>
        <label class="form-check-label" for="">
-       <div>
-           <p>${ingredient.product}<p>
-           <p>${ingredient.brand}<p>
+       <div >
+           <div >
+           <div class="js-ingredient_name">${ingredient.product}</div>
+           <div>${ingredient.brand}<div>
            <span>${ingredient.quantity}</span>
            </div>
        </label>
-       <p class="">${ingredient.price} ${currency}</p>
+       <div class="js-price">${ingredient.price}<small> ${currency}</small></div>
        `
         const newItem = document.createElement("div");
-        newItem.setAttribute("class", "form-check flex-row justify-content-center align-items-center");
+        newItem.setAttribute("class", "form-check flex-row flex-nowrap justify-content-center align-items-center selecteditem js-ingredients");
         newItem.innerHTML = html;
         mixIngredientsList.appendChild(newItem)
     })
 }
 
+function readCheckboxesItemsandQuantity() {
+    let partialItemsAndQuantity = []
+    let ingredientsElement = document.querySelectorAll(".js-ingredients");
+    for (let ingredient of ingredientsElement) {
+        let isChecked = ingredient.querySelector(".js-mix-check").checked;
+        if (isChecked) {
+            let productName = ingredient.querySelector(".js-ingredient_name").textContent;
+            let quantityInput = parseInt(ingredient.querySelector(".js-quantity").value);
+            partialItemsAndQuantity.push({ [productName]: quantityInput })
+        }
+
+    }
+    console.log(partialItemsAndQuantity)
+}
+
 // Init 
 getRecipes()
-
-
-// function getRecipes() {
-//     return fetch(recipesPath)
-//         .then(resp => resp.json())
-//         .then(data => {
-//             console.log(data)
-//             for (let recipe in data) {
-//                 availableRecipes.push(data[recipe])
-//             }
-//             return availableRecipes
-//         })
-//         //.then(next => console.log(next))
-//         .catch(err => console.log(err));
-
-// }
-// function paintMixforEachRecipe(recipes) {
-//     console.log(recipes)
-//     //     debugger;
-//     //     for(let recipe in recipes) {
-//     //         console.log(recipe)
-//     //     }
-// }
-
-// getRecipes();
-// if (availableRecipes.length > 0) {
-//     paintMixforEachRecipe(availableRecipes)
-// }
-// //.then(res => paintMixforEachRecipe(res));
-
